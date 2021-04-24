1c4ac3529f155f9d98774c2de283a25923dea97a
ajav06 <albert06av@hotmail.com>
2021-04-24 19:22:00 -0400

    init

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        primary: '#6F9F77',
+        secundary:'#9dbea3',
+        background: '#0F444C',
+        'background-title': '#5e8d83',
+        title: '#e8ae1c',
+        'sub-title': '#27343a',
+        active: '#f4f5f6',
+      }
+    },
+    fontFamily: {
+      comfortaa: ['Comfortaa'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
