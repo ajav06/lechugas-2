f7ae91f7543fa5957d67b30b6e476497aa39c442
Anyi <anyeliv02@gmail.com>
2021-04-24 22:02:08 -0400

    apply

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,23 +3,23 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors:{
-        primary: '#6F9F77',
-        secundary:'#9dbea3',
-        background: '#0F444C',
-        'background-title': '#5e8d83',
-        title: '#e8ae1c',
-        'sub-title': '#27343a',
-        active: '#f4f5f6',
-      }
+      colors: {
+        primary: "#6F9F77",
+        secundary: "#9dbea3",
+        background: "#0F444C",
+        "background-title": "#5e8d83",
+        title: "#e8ae1c",
+        "sub-title": "#27343a",
+        active: "#f4f5f6",
+        grey: "#746868",
+      },
     },
     fontFamily: {
-      comfortaa: ['Comfortaa'],
+      comfortaa: ["Comfortaa"],
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
-
+};
