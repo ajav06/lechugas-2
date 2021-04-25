ac2481bc432111e7ef3a8ccfa06390e6a9efd836
ajav06 <albert06av@hotmail.com>
2021-04-24 21:10:39 -0400

    fix

--- src/components/Suppliers.svelte
+++ src/components/Suppliers.svelte
@@ -1,12 +1,20 @@
 <main>
-  <div class="grid grid-cols-7 md:grid-cols-1 bg-primary rounded-r-lg ">
+  <div
+    class="grid grid-cols-7 md:grid-cols-1 bg-primary rounded-b-lg md:rounded-b-none md:rounded-r-lg">
     <div
-      class="bg-background-title col-span-12 text-title font-comfortaa md:h-10 flex justify-center rounded-tr-lg">
-      <span class="self-center">Proveedores</span>
+      class="bg-background-title col-span-12 md:h-10 flex justify-center md:rounded-tr-lg">
+      <span
+        class="self-center text-title font-comfortaa-semibol tracking-wider">
+        Proveedores
+      </span>
     </div>
-    <div class="md:col-span-12">Dolar Today</div>
-    <div class="md:col-span-12">Yadio</div>
-    <div class="md:col-span-12">BCV</div>
-    <div class="md:col-span-12">Monitor Dolar</div>
+    <div class="md:col-span-12 md:h-10 flex justify-center">
+      <span class="self-center font-comfortaa-semibol tracking-wider">
+        Dolar Today
+      </span>
+    </div>
+    <div class="md:col-span-12 md:h-10 flex justify-center">Yadio</div>
+    <div class="md:col-span-12 md:h-10 flex justify-center">BCV</div>
+    <div class="md:col-span-12 md:h-10 flex justify-center">Monitor Dolar</div>
   </div>
 </main>
