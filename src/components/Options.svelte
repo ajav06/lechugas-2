7ed6d2a9715b2ce22bea9251ecb667e5f3ef8371
Anyi <anyeliv02@gmail.com>
2021-04-24 22:44:57 -0400

    options v.2

--- src/components/Options.svelte
+++ src/components/Options.svelte
@@ -1,26 +1,29 @@
 <main>
   <div
-    class="grid grid-cols-4 md:grid-cols-1 bg-primary rounded-t-lg md:rounded-b-none md:rounded-l-lg">
+    class="grid grid-cols-3 md:grid-cols-1 bg-primary rounded-t-lg md:rounded-b-none md:rounded-l-lg">
     <div
-      class="bg-background-title col-span-12 md:h-10 flex justify-center rounded-t-lg md:rounded-tl-lg md:rounded-tr-none">
+      class="bg-background-title col-span-12 md:h-16 flex justify-center rounded-t-lg md:rounded-tl-lg md:rounded-tr-none">
       <span
-        class="self-center text-title font-comfortaa-semibol tracking-wider">
+        class="self-center text-title text-xl font-comfortaa-semibol tracking-wider">
         Opciones
       </span>
     </div>
-    <div class="md:col-span-12 md:h-10 flex justify-center borders-left">
+    <div class=" md:col-span-12 md:h-16 flex justify-center borders-left">
       <span class="self-center font-comfortaa-semibol tracking-wider">
         Consultar
       </span>
     </div>
     <div
-      class="md:col-span-12 md:h-10 bg-secundary flex justify-center borders-left">
-      Convertir
+      class=" md:col-span-12 md:h-16 bg-secundary flex justify-center borders-left">
+      <span class="self-center font-comfortaa-semibol tracking-wider">
+        Convertir
+      </span>
     </div>
     <div
-      class="md:col-span-12 md:h-10 bg-secundary flex justify-center borders-left">
-      Historial
+      class=" md:col-span-12 md:h-16 bg-secundary flex justify-center borders-left">
+      <span class="self-center font-comfortaa-semibol tracking-wider">
+        Historial
+      </span>
     </div>
-    <div class="md:col-span-12 md:h-10 bg-secundary flex justify-center" />
   </div>
 </main>
