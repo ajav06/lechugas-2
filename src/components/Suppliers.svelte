1dde7300a3706fd146edcf9e89bc7ccdd3b02074
ajav06 <albert06av@hotmail.com>
2021-04-24 22:44:31 -0400

    change

--- src/components/Suppliers.svelte
+++ src/components/Suppliers.svelte
@@ -1,49 +1,33 @@
 <main>
-  <div
-    class="grid grid-cols-7 md:grid-cols-1 bg-secundary rounded-b-lg md:rounded-b-none md:rounded-r-lg">
-    <div
-      class="bg-background-title col-span-12 h-10 flex justify-center md:rounded-tr-lg">
-      <strong
-        class="self-center text-title font-comfortaa-semibol tracking-wider">
+  <div class="options">
+    <div class="title-suppliers">
+      <strong class="text-suppliers font-comfortaa-semibol">
         Proveedores
       </strong>
     </div>
-    <div
-      class="md:col-span-12 md:h-16 flex justify-center space-x-2 border border-l-0 border-grey">
-      <img src="./images/DolarToday.svg" alt="" class="h-8 self-center" />
-      <span class="self-center font-comfortaa-semibol tracking-wider">
-        Dolar Today
-      </span>
+    <div class="options-suppliers borders-right">
+      <img src="./images/DolarToday.svg" alt="" class="img-suppliers" />
+      <span class="text-options font-comfortaa-semibol"> Dolar Today </span>
     </div>
-    <div
-      class="md:col-span-12 md:h-16 flex justify-center space-x-2 border border-l-0 border-grey">
-      <span class="self-center font-comfortaa-semibol tracking-wider">
-        Yadio
-      </span>
+    <div class="options-suppliers borders-right">
+      <img src="./images/Yadio.svg" alt="" class="img-suppliers" />
+      <span class="text-options font-comfortaa-semibol"> Yadio </span>
     </div>
-    <div
-      class="md:col-span-12 md:h-16 flex justify-center space-x-2 border border-l-0 border-grey">
-      <span class="self-center font-comfortaa-semibol tracking-wider">
-        BCV
-      </span>
+    <div class="options-suppliers borders-right">
+      <img src="./images/BCV.svg" alt="" class="img-suppliers" />
+      <span class="text-options font-comfortaa-semibol"> BCV </span>
     </div>
-    <div
-      class="md:col-span-12 md:h-16 flex justify-center space-x-2 border border-l-0 border-grey">
-      <span class="self-center font-comfortaa-semibol tracking-wider">
-        Monitor Dolar
-      </span>
+    <div class="options-suppliers borders-right">
+      <img src="./images/MonitorDolar.svg" alt="" class="img-suppliers" />
+      <span class="text-options font-comfortaa-semibol"> Monitor Dolar </span>
     </div>
-    <div
-      class="md:col-span-12 md:h-16 flex justify-center space-x-2 border border-l-0 border-grey">
-      <span class="self-center font-comfortaa-semibol tracking-wider">
-        LocalBitcoins
-      </span>
+    <div class="options-suppliers borders-right">
+      <img src="./images/LocalBitcoins.svg" alt="" class="img-suppliers" />
+      <span class="text-options font-comfortaa-semibol"> LocalBitcoins </span>
     </div>
-    <div
-      class="md:col-span-12 md:h-16 flex justify-center space-x-2 border border-l-0 border-grey">
-      <span class="self-center font-comfortaa-semibol tracking-wider">
-        Promedio
-      </span>
+    <div class="options-suppliers">
+      <img src="./images/DolarToday.svg" alt="" class="img-suppliers" />
+      <span class="text-options font-comfortaa-semibol"> Promedio </span>
     </div>
   </div>
 </main>
