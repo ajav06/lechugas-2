c8a7386b6da274684f5f70f6d7ef069484e36de5
ajav06 <albert06av@hotmail.com>
2021-04-24 20:12:32 -0400

    prettier

--- src/Index.svelte
+++ src/Index.svelte
@@ -1,13 +1,14 @@
-<main>
-    <div class="container px-auto pt-5">
-        <div class="flex justify-center">
-            <img src="./images/logo.png" alt="Lechugas^2" class="box-content w-60 md:w-96" />
-        </div>
+<main class="container px-auto pt-5">
+  <div class="flex justify-center">
+    <img
+      src="./images/logo.png"
+      alt="Lechugas^2"
+      class="box-content w-60 md:w-96" />
+  </div>
 
-        <div class="grid grid-cols-1 md:grid-cols-4 md:mx-10 my-5 justify-center">
-            <div class="bg-primary">1</div>
-            <div class="bg-primary md:col-span-2">2</div>
-            <div class="bg-primary">3</div>
-        </div>
-    </div>
+  <div class="grid grid-cols-1 md:grid-cols-4 md:mx-10 my-5">
+    <div class="bg-primary">1</div>
+    <div class="bg-primary md:col-span-2">2</div>
+    <div class="bg-primary">3</div>
+  </div>
 </main>
