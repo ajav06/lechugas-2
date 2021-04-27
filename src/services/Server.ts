6075afc1568bbdb92407a978a92c44399d8e86e2
ajav06 <albert06av@hotmail.com>
2021-04-26 23:22:39 -0400

    update 2.0

--- src/services/Server.ts
+++ src/services/Server.ts
@@ -1,6 +1,7 @@
 // import { Application } from 'https://deno.land/x/oak/mod.ts'
 import { Database } from 'https://deno.land/x/denodb/mod.ts'
 import opine, { serveStatic } from 'https://deno.land/x/opine@1.3.3/mod.ts'
+import { parse } from 'https://deno.land/std/flags/mod.ts'
 
 import { db } from './postgresql.ts'
 import { router } from '../routes/index.ts'
@@ -10,6 +11,11 @@ import {
   consultarLocalBitcoin,
   consultarMonitorDolar
 } from '../components/dolar/controller.ts'
+
+const { args } = Deno
+const DEFAULT_PORT = 8000
+const argPort = parse(args).port
+const port = argPort ? Number(argPort) : DEFAULT_PORT
 
 export class Server {
   app: any
@@ -31,7 +37,7 @@ export class Server {
   }
 
   listen() {
-    this.app.listen({ port: 8080 })
-    console.log('Server running at http://localhost:8080')
+    this.app.listen({ port })
+    console.log(`Server running at http://localhost:${port}`)
   }
 }
