c865e808749fd64fdf0d34bd9eea7acd71659f7a
ajav06 <albert06av@hotmail.com>
2021-04-27 22:00:44 -0400

    cors

--- src/services/Server.ts
+++ src/services/Server.ts
@@ -2,6 +2,7 @@
 import { Database } from 'https://deno.land/x/denodb/mod.ts'
 import opine, { serveStatic } from 'https://deno.land/x/opine@1.3.3/mod.ts'
 import { parse } from 'https://deno.land/std/flags/mod.ts'
+import { opineCors } from 'https://deno.land/x/cors/mod.ts'
 
 import { db } from './postgresql.ts'
 import { router } from '../routes/index.ts'
@@ -32,6 +33,7 @@ export class Server {
   }
 
   routes() {
+    this.app.use(opineCors())
     this.app.use('/api/v1/', router)
     this.app.use('/public/', serveStatic(`${Deno.cwd()}/public/`))
   }
