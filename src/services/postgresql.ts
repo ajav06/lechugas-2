7e4725e7e436ccb83f369e47b5f95c6f15d86a2f
AJAV06 <albert06av@outlook.com>
2021-10-02 16:13:38 -0400

    postgrest

--- src/services/postgresql.ts
+++ src/services/postgresql.ts
@@ -3,9 +3,9 @@ import { Client } from 'https://deno.land/x/postgres/mod.ts'
 
 import { Dolar } from '../components/dolar/model.ts'
 import { Proveedor } from '../components/proveedor/model.ts'
-import { config } from 'https://deno.land/x/dotenv/mod.ts'
+// import { config } from 'https://deno.land/x/dotenv/mod.ts'
 
-const env = config()
+const env = Deno.env.toObject()
 
 const connector = new PostgresConnector({
   database: env.DATABASE,
